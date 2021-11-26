8baebb3425cf3875d1406920a1be7f7691b8606e
NikoBilicic <w0528921@myscc.ca>
2021-11-26 10:59:28 -0500

    #2 began building dedicated functions to retrieve poke info

--- src/main/java/com/example/maddexjavafinal/GsonFunc.java
+++ src/main/java/com/example/maddexjavafinal/GsonFunc.java
@@ -1,5 +1,60 @@
 package com.example.maddexjavafinal;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+
 public class GsonFunc {
 
+    //Function to get pokemon name
+    public static String getPokeName(JsonObject object) {
+        String pokeName = "";
+        pokeName = String.valueOf(object.get("name"));
+        return pokeName;
+    }
+
+    //Function to get pokemon Dex num
+    public static int getDexNum(JsonObject object) {
+        int dexNum = 0;
+        dexNum = object.get("id").getAsInt();
+        return dexNum;
+    }
+
+    public static String getPokeTyping(JsonObject object) {
+        JsonArray types = object.getAsJsonArray("types");
+        if (types.size() > 1) {
+            JsonObject type1 = (JsonObject) types.get(0);
+            JsonObject type1Type = type1.getAsJsonObject("type");
+            types.add(String.valueOf(type1Type.get("name")));
+            JsonObject type2 = (JsonObject) types.get(1);
+            JsonObject type2Type = type2.getAsJsonObject("type");
+            String[] pokeTypes = {String.valueOf(type1Type.get("name")), String.valueOf(type2Type.get("name"))};
+            return pokeTypes.toString();
+        } else {
+            JsonObject type1 = (JsonObject) types.get(0);
+            JsonObject type1Type = type1.getAsJsonObject("type");
+            String pokeTypes = String.valueOf(type1Type.get("name"));
+            return pokeTypes;
+        }
+    }
+
+    public static Integer getPokeGen(int dexNum) {
+        if (dexNum >= 1 && dexNum <= 151) {
+            return 1;
+        } else if (dexNum >= 152 && dexNum <= 251) {
+            return  2;
+        } else if (dexNum >= 252 && dexNum <= 386) {
+            return  3;
+        } else if (dexNum >= 387 && dexNum <= 493) {
+            return  4;
+        } else if (dexNum >= 494 && dexNum <= 649) {
+            return 5;
+        } else if (dexNum >= 650 && dexNum <= 721) {
+            return 6;
+        } else if (dexNum >= 722 && dexNum <= 809) {
+            return 7;
+        } else if (dexNum >= 810 && dexNum <= 898) {
+            return 8;
+        }
+        return null;
+    }
 }
--- src/main/java/com/example/maddexjavafinal/HelloApplication.java
+++ src/main/java/com/example/maddexjavafinal/HelloApplication.java
@@ -26,6 +26,8 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
+import static com.example.maddexjavafinal.GsonFunc.*;
+
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
@@ -37,72 +39,27 @@ public class HelloApplication extends Application {
 
         //Build Pokémon
         HttpURLConnection connection = (HttpURLConnection)
-                new URL("https://pokeapi.co/api/v2/pokemon/898").openConnection();
+                new URL("https://pokeapi.co/api/v2/pokemon/ditto").openConnection();
         int dexNum = 0;
-        String pokeName = null;
-        String type1Name = null;
-        String type2Name = null;
         int gen = 0;
         if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
             JsonReader reader = new JsonReader(new InputStreamReader(connection.getInputStream()));
             JsonElement element = JsonParser.parseReader(reader);
             JsonObject object = element.getAsJsonObject();
             //name
-            pokeName = String.valueOf(object.get("name"));
-            System.out.println(pokeName);
+            System.out.println(getPokeName(object));
             //dex number
-            JsonElement id = object.get("id");
-            dexNum = id.getAsInt();
-            System.out.println(dexNum);
+            System.out.println(getDexNum(object));
             //types
-            JsonArray types = object.getAsJsonArray("types");
-            if (types.size() > 1) {
-                JsonObject type1 = (JsonObject) types.get(0);
-                JsonObject type1Type = type1.getAsJsonObject("type");
-                type1Name = String.valueOf(type1Type.get("name"));
-                JsonObject type2 = (JsonObject) types.get(1);
-                JsonObject type2Type = type2.getAsJsonObject("type");
-                type2Name = String.valueOf(type2Type.get("name"));
-
-                System.out.println(type1Name + " " + type2Name);
-            } else {
-                JsonObject type1 = (JsonObject) types.get(0);
-                JsonObject type1Type = type1.getAsJsonObject("type");
-                type1Name = String.valueOf(type1Type.get("name"));
-
-                System.out.println(type1Name);
-            }
+            System.out.println(getPokeTyping(object));
             //Generation
-            if (dexNum >= 1 && dexNum <= 151) {
-                System.out.println("Gen 1");
-                gen = 1;
-            } else if (dexNum >= 152 && dexNum <= 251) {
-                System.out.println("Gen 2");
-                gen = 2;
-            } else if (dexNum >= 252 && dexNum <= 386) {
-                System.out.println("Gen 3");
-                gen = 3;
-            } else if (dexNum >= 387 && dexNum <= 493) {
-                System.out.println("Gen 4");
-                gen = 4;
-            } else if (dexNum >= 494 && dexNum <= 649) {
-                System.out.println("Gen 5");
-                gen = 5;
-            } else if (dexNum >= 650 && dexNum <= 721) {
-                System.out.println("Gen 6");
-                gen = 6;
-            } else if (dexNum >= 722 && dexNum <= 809) {
-                System.out.println("Gen 7");
-                gen = 7;
-            } else if (dexNum >= 810 && dexNum <= 898) {
-                System.out.println("Gen 8");
-                gen = 8;
-            }
-
+            System.out.println(getPokeGen(getDexNum(object)));
+            //object
+            Poke poke = new Poke(getDexNum(object), "Sprite", getPokeName(object), getPokeTyping(object), getPokeGen(getDexNum(object)));
+            System.out.println(poke);
         }
 
-        Poke poke = new Poke(dexNum, "Sprite", pokeName, type1Name, gen);
-        System.out.println(poke);
+
 
         BorderPane root = new BorderPane();
 
